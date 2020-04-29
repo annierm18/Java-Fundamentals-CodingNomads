100b07910dd6f1afbc3579569a5095d7a9210e4c
annierm18 <annierm18@yahoo.com>
2020-04-28 19:37:34 -0700

    Added a main class and a package.

--- src/labs_examples/fundamentals/labs/Main.java
+++ src/labs_examples/fundamentals/labs/Main.java
@@ -10,15 +10,16 @@ package labs_examples.fundamentals.labs;
  *
  */
 
-public class Exercise_03 {
+public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
-	
-	System.out.println("Annie test");
+        System.out.println("Hello World!");
+        System.out.println("Check out my first program!");
+	    System.out.println("Annie test");
     }
 
 }
